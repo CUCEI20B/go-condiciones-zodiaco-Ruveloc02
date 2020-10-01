8d195c6d5ee7eda504d0ac26d5e3b1ed6b430059
Carlos Ruiz Velasco Lopez <carlos.ruizvelasco2917@alumnos.udg.mx>
2020-09-30 20:09:39 -0500

    Cambié mayúsculas por minúsculas.

--- main.go
+++ main.go
@@ -9,28 +9,28 @@ func main() {
 	fmt.Scan(&dia)
 	fmt.Scan(&mes)
 	if (mes == 1 && dia >= 21 && dia <= 31) || (mes == 2 && dia >= 1 && dia <= 19) {
-		fmt.Print("Acuario")
+		fmt.Print("acuario")
 	} else if (mes == 2 && dia >= 20 && dia <= 28) || (mes == 3 && dia >= 1 && dia <= 20) {
-		fmt.Print("Piscis")
+		fmt.Print("piscis")
 	} else if (mes == 3 && dia >= 21 && dia <= 31) || (mes == 4 && dia >= 1 && dia <= 20) {
-		fmt.Print("Aries")
+		fmt.Print("aries")
 	} else if (mes == 4 && dia >= 21 && dia <= 30) || (mes == 5 && dia >= 1 && dia <= 21) {
-		fmt.Print("Tauro")
+		fmt.Print("tauro")
 	} else if (mes == 5 && dia >= 22 && dia <= 31) || (mes == 6 && dia >= 1 && dia <= 21) {
-		fmt.Print("Geminis")
+		fmt.Print("geminis")
 	} else if (mes == 6 && dia >= 22 && dia <= 30) || (mes == 7 && dia >= 1 && dia <= 23) {
-		fmt.Print("Cancer")
+		fmt.Print("cancer")
 	} else if (mes == 7 && dia >= 24 && dia <= 31) || (mes == 8 && dia >= 1 && dia <= 23) {
-		fmt.Print("Leo")
+		fmt.Print("leo")
 	} else if (mes == 8 && dia >= 24 && dia <= 31) || (mes == 9 && dia >= 1 && dia <= 23) {
-		fmt.Print("Virgo")
+		fmt.Print("virgo")
 	} else if (mes == 9 && dia >= 24 && dia <= 30) || (mes == 10 && dia >= 1 && dia <= 23) {
-		fmt.Print("Libra")
+		fmt.Print("libra")
 	} else if (mes == 10 && dia >= 24 && dia <= 31) || (mes == 11 && dia >= 1 && dia <= 22) {
-		fmt.Print("Escorpio")
+		fmt.Print("escorpio")
 	} else if (mes == 11 && dia >= 23 && dia <= 31) || (mes == 12 && dia >= 1 && dia <= 21) {
-		fmt.Print("Sagitario")
+		fmt.Print("sagitario")
 	} else if (mes == 12 && dia >= 22 && dia <= 31) || (mes == 1 && dia >= 1 && dia <= 20) {
-		fmt.Print("Capricornio")
+		fmt.Print("capricornio")
 	}
 }
